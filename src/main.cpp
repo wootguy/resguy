37404635f15c2cc8670a7485b6b39229441d1c9b
wootguy <drake.rhunter@gmail.com>
2017-07-11 11:46:17 -0700

    oopsies

--- src/main.cpp
+++ src/main.cpp
@@ -850,7 +850,7 @@ string bsp_name(string fname)
 	return f;
 }
 
-string version_string = "gsrand v5 (July 2017)";
+string version_string = "resguy v5 (July 2017)";
 
 int main(int argc, char* argv[])
 {
