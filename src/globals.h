56e7d272f4eaa53d0572a38aef84ad6cf114fe81
Drake Hunter <hundrake@amazon.com>
2017-11-21 16:22:04 -0800

    Allow "sound/" prefix to be missing from string literals
    This will fix false positives which are difficult to fix (an AS parser/compiler would be required), but will hide cases where the mapper really should have added the prefix.

--- src/globals.h
+++ src/globals.h
@@ -20,6 +20,7 @@ static const char * g_valid_exts[NUM_VALID_EXTS] = {
 
 extern str_map_vector default_wads;
 extern vector<string> server_files; // files used by server but not needed by client
+extern vector<string> default_content; // files included with the base game
 extern int unused_wads;
 extern bool client_files_only;
 extern bool print_skip;
