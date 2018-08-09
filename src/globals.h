bcebb6e579c8ff11c0b2d2f53fe80d7571602a17
wootguy <w00tguy123@gmail.com>
2018-08-09 01:55:58 -0700

    Don't include blacklisted audio formats (fixes #10)

--- src/globals.h
+++ src/globals.h
@@ -6,11 +6,11 @@
 #define NUM_VALID_EXTS (11 + NUM_SOUND_EXTS)
 static const char * g_valid_exts[NUM_VALID_EXTS] = {
 	// sound files
-	"aiff", "asf", "asx", "dls", "flac", 
-	"fsb", "it", "m3u", "midi", "mid", 
-	"mod", "mp2", "pls", "s3m", "vag", 
-	"wax", "wma", "xm", "wav", "ogg", 
-	"mp3", "au",
+	"asf", "asx", "dls", "fsb",  "m3u", "mid", "mod", "mp2", "pls", "s3m", 
+	"vag", "wax", "wma", "wav", "ogg", "mp3", "au",
+
+	// Blacklisted in sven (but shouldn't be):
+	// "aiff", "flac", "it", "midi", "xm",
 
 	// everything else
 	"mdl", "spr", "res", "txt", "tga", 
